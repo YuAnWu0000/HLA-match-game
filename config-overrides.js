f70a22f3770f0012040b0c74e4319648db13b8b5
zachary <andywu03242000@gmail.com>
2024-09-13 16:29:52 +0800

    FIX: Resolver image url

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,17 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = function override(config) {
+  const targetRegex = /\.css$/
+  const targetPackage = /css-loader/
+  // to make url() in css worked
+  config.module.rules
+    .find((r) => r.oneOf)
+    .oneOf.find((r) => r.test.toString() === targetRegex.toString())
+    .use.find((o) => o.loader && targetPackage.test(o.loader)).options.url =
+    false
   config.resolve.alias = {
     ...config.resolve.alias,
-    "@": path.resolve(__dirname, "./src"),
-  };
-  return config;
-};
+    '@': path.resolve(__dirname, './src')
+  }
+  return config
+}
