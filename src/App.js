873bf2c4666837081944fdb63eb3f7956a8af48f
zachary <andywu03242000@gmail.com>
2024-09-16 17:04:33 +0800

    UPDATE: Background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-cover bg-[url('/images/bg.jpg')]"></div>
+    <div className="w-full h-screen bg-cover bg-[center_-10rem] bg-[url('/images/bg4.jpg')]"></div>
   )
 }
 
