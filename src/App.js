c867d5c37b669f0eb5363dedef9a16b798aaa95c
zachary <andywu03242000@gmail.com>
2024-09-19 16:32:32 +0800

    UPDATE: Country background

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="w-full h-screen [perspective:1000px;]">
       <div
-        className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:200%_200%] bg-repeat
-          bg-[center_-30rem] bg-[url('/images/bg_0.jpg')]"
+        className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:160%_160%] bg-repeat
+          bg-[center_-20rem] bg-[url('/images/bg_1.jpg')]"
       ></div>
       {/* <img
         src="/images/bg_0.jpg"
@@ -12,7 +12,7 @@ function App() {
         alt=""
       /> */}
       <div
-        className="relative top-[-2rem] w-[80rem] h-[52rem] mx-auto flex justify-center
+        className="relative top-[-6rem] w-[80rem] h-[52rem] mx-auto flex justify-center
           content-center flex-wrap gap-6 [transform-style:preserve-3d;]
           [transform:rotateX(35deg);]"
       >
