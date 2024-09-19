9446f334b7b5d1640f888fc254a4ac702ddb23c1
zachary <andywu03242000@gmail.com>
2024-09-19 15:13:47 +0800

    ADD: Card board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-cover bg-[center_-10rem] bg-[url('/images/bg4.jpg')]"></div>
+    <div
+      className="w-full h-screen bg-[length:200%_200%] bg-no-repeat bg-[center_-30rem]
+        bg-[url('/images/bg_0.jpg')]"
+    >
+      <div
+        className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 w-[60rem] h-[60vh]
+          flex justify-center flex-wrap gap-4"
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+          <div
+            key={`dna_card_${item}`}
+            className="w-[10rem] h-[15rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"
+          ></div>
+        ))}
+      </div>
+    </div>
   )
 }
 
