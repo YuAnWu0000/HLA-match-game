02a4f6608b329c3f2fa8010ba5c4567001689079
zachary <andywu03242000@gmail.com>
2024-09-19 15:54:46 +0800

    UPDATE: Card board rotation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 function App() {
   return (
-    <div
-      className="w-full h-screen bg-[length:200%_200%] bg-no-repeat bg-[center_-30rem]
-        bg-[url('/images/bg_0.jpg')]"
-    >
+    <div className="w-full h-screen [perspective:500px;]">
       <div
-        className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 w-[60rem] h-[60vh]
-          flex justify-center flex-wrap gap-4"
+        className="w-full h-full bg-[length:200%_200%] bg-no-repeat bg-[center_-30rem]
+          bg-[url('/images/bg_0.jpg')] [perspective:500px;]"
       >
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <div
-            key={`dna_card_${item}`}
-            className="w-[10rem] h-[15rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"
-          ></div>
-        ))}
+        <div
+          className="w-[60rem] h-[45rem] m-auto flex justify-center content-center flex-wrap gap-4
+            [transform-style:preserve-3d;] [transform:rotateX(25deg);]"
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+            <div
+              key={`dna_card_${item}`}
+              className="w-[10rem] h-[15rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
   )
