9ae8cec87e2e3ff577fdd859b778b47c23ccea69
zachary <andywu03242000@gmail.com>
2024-09-20 08:53:37 +0800

    UPDATE: Adjust background ratio

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="w-full h-screen [perspective:1000px;]">
       <div
-        className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:160%_160%] bg-repeat
+        className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:140%_auto] bg-repeat
           bg-[center_-20rem] bg-[url('/images/bg_1.jpg')]"
       ></div>
       {/* <img
@@ -12,8 +12,8 @@ function App() {
         alt=""
       /> */}
       <div
-        className="relative top-[-6rem] w-[80rem] h-[52rem] mx-auto flex justify-center
-          content-center flex-wrap gap-6 [transform-style:preserve-3d;]
+        className="relative top-[-2rem] w-[80rem] h-[52rem] mx-auto flex justify-center
+          content-center flex-wrap gap-8 [transform-style:preserve-3d;]
           [transform:rotateX(35deg);]"
       >
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
