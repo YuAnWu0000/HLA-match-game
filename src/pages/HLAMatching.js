1e0f7fc97a4d33579cf5f2c822962fe1e18df20d
zachary <andywu03242000@gmail.com>
2024-09-23 16:46:54 +0800

    UPDATE: HLA matching page

--- src/pages/HLAMatching.js
+++ src/pages/HLAMatching.js
@@ -5,6 +5,11 @@ function HLAMatching() {
         className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:140%_auto] bg-repeat
           bg-[center_-20rem] bg-[url('/images/bg_country.jpg')]"
       ></div>
+      <div
+        className="relative top-[-2rem] w-[80rem] h-[52rem] mx-auto flex justify-center
+          content-center flex-wrap gap-8 [transform-style:preserve-3d;]
+          [transform:rotateX(35deg);]"
+      ></div>
     </div>
   )
 }
