9a6e05a7352af49e1f64b52765bf40d9bf41ae8b
zachary <andywu03242000@gmail.com>
2024-09-23 16:46:35 +0800

    UPDATE: HLA matching page

--- src/pages/HLAMatching.js
+++ src/pages/HLAMatching.js
@@ -1,5 +1,12 @@
 function HLAMatching() {
-  return <div className="w-full h-screen [perspective:1000px;]"></div>
+  return (
+    <div className="w-full h-screen [perspective:1000px;]">
+      <div
+        className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:140%_auto] bg-repeat
+          bg-[center_-20rem] bg-[url('/images/bg_country.jpg')]"
+      ></div>
+    </div>
+  )
 }
 
 export default HLAMatching
