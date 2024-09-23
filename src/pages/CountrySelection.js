782bfa507be5c35b5f22072a3f60c550796428cd
zachary <andywu03242000@gmail.com>
2024-09-23 16:41:42 +0800

    UPDATE: Country selection page

--- src/pages/CountrySelection.js
+++ src/pages/CountrySelection.js
@@ -5,6 +5,12 @@ function CountrySelection() {
         className="absolute top-0 right-0 -z-50 w-full h-screen bg-[length:140%_auto] bg-repeat
           bg-[center_-20rem] bg-[url('/images/bg_country.jpg')]"
       ></div>
+      {/* <img
+        src="/images/bg_0.jpg"
+        className="absolute top-0 right-0 -z-50 w-full h-auto [transform-style:preserve-3d;]
+          [transform:rotateX(-60deg);]"
+        alt=""
+      /> */}
     </div>
   )
 }
