863171f542e4229d72cfda39e4d78e2ba860ae37
zachary <andywu03242000@gmail.com>
2024-09-23 16:42:29 +0800

    UPDATE: Country selection page

--- src/pages/CountrySelection.js
+++ src/pages/CountrySelection.js
@@ -15,7 +15,18 @@ function CountrySelection() {
         className="relative top-[-2rem] w-[80rem] h-[52rem] mx-auto flex justify-center
           content-center flex-wrap gap-8 [transform-style:preserve-3d;]
           [transform:rotateX(35deg);]"
-      ></div>
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+          // <div
+          //   key={`dna_card_${item}`}
+          //   className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"
+          // ></div>
+          <div
+            key={`country_card_${item}`}
+            className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]"
+          ></div>
+        ))}
+      </div>
     </div>
   )
 }
