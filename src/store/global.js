3919f7d7893bf3f185f94e39e51057a43982fb48
zachary <andywu03242000@gmail.com>
2024-09-24 08:51:33 +0800

    UPDATE: Global store

--- src/store/global.js
+++ src/store/global.js
@@ -1,4 +1,7 @@
 import { create } from 'zustand'
 
-const useGlobalStore = create((set, get) => ({}))
+const useGlobalStore = create((set, get) => ({
+  isLoading: false,
+  isDialogOpen: false
+}))
 export default useGlobalStore
