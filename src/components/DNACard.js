102c908475c9d3b349ee6fac61213ea9928644e6
zachary <andywu03242000@gmail.com>
2024-09-24 08:42:35 +0800

    UPDATE: DNA card component

--- src/components/DNACard.js
+++ src/components/DNACard.js
@@ -1,4 +1,6 @@
 function DNACard() {
-  return <></>
+  return (
+    <div className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"></div>
+  )
 }
 export default DNACard
