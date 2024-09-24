41593d263a201211e3f6a285a270f38610184f6b
zachary <andywu03242000@gmail.com>
2024-09-24 08:52:37 +0800

    UPDATE: Global store

--- src/store/global.js
+++ src/store/global.js
@@ -2,6 +2,12 @@ import { create } from 'zustand'
 
 const useGlobalStore = create((set, get) => ({
   isLoading: false,
-  isDialogOpen: false
+  isDialogOpen: false,
+  customDialog: {
+    name: '',
+    props: {}
+  },
+  setCustomDialog: (name, data) =>
+    set((state) => ({ customDialog: { name, props: data ? data : {} } }))
 }))
 export default useGlobalStore
