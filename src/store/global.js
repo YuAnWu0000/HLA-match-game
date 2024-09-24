f40bea60fde6e815c7c5000bf2029e7e5c87d67c
zachary <andywu03242000@gmail.com>
2024-09-24 08:53:43 +0800

    UPDATE: Global store

--- src/store/global.js
+++ src/store/global.js
@@ -8,6 +8,26 @@ const useGlobalStore = create((set, get) => ({
     props: {}
   },
   setCustomDialog: (name, data) =>
-    set((state) => ({ customDialog: { name, props: data ? data : {} } }))
+    set((state) => ({ customDialog: { name, props: data ? data : {} } })),
+  dialogData: {
+    title: '',
+    body: '',
+    buttons: [
+      {
+        name: '取消',
+        variant: 'secondary',
+        event: () => {}
+      },
+      {
+        name: '確認',
+        variant: 'primary',
+        event: () => {}
+      }
+    ]
+  },
+  setDialogData: (data) => {
+    get().setCustomDialog('', {})
+    return set((state) => ({ dialogData: data }))
+  }
 }))
 export default useGlobalStore
