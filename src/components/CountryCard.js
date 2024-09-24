0d5d8cc768975e00fa63d1c3d61136c5ae9e53f1
zachary <andywu03242000@gmail.com>
2024-09-24 08:46:04 +0800

    UPDATE: Country card component

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,5 +1,7 @@
 function CountryCard() {
-  return <></>
+  return (
+    <div className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]"></div>
+  )
 }
 
 export default CountryCard
