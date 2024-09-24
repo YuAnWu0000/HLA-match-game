d3510072588645c7db62cb370d0bb401009722b1
zachary <andywu03242000@gmail.com>
2024-09-24 09:05:03 +0800

    ADD: Dynamic component for game flow

--- src/store/game.js
+++ src/store/game.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 const useGameStore = create((set, get) => ({
   money: 1000000,
-  flow: 'COUNTRY_SELECTION',
+  flow: 'COUNTRY_SELECTION', // HLA_MATCHING
   setGameFlow: (flow) => set(() => ({ flow }))
 }))
 export default useGameStore
