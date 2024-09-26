1d0ab42248d46c71391675d8952213946961b99a
zachary <andywu03242000@gmail.com>
2024-09-26 11:28:03 +0800

    UPDATE: Country card order animation

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types'
 import { twMerge } from 'tailwind-merge'
 
 function CountryCard({ order, className }) {
-  // [--delayTime:0s], [--delayTime:1s], [--delayTime:2s], [--delayTime:3s], [--delayTime:4s]
+  // [--delayTime:1s], [--delayTime:2s], [--delayTime:3s], [--delayTime:4s], [--delayTime:5s]
   return (
     <div
       className={twMerge(
         `w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]
         [transform-style:preserve-3d;] translate-y-[-50rem] animate-cardDrop
-        [--delayTime:${order - 1}s]`,
+        [--delayTime:${order}s]`,
         className
       )}
     ></div>
