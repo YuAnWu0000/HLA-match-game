ff237cf1d6697694db2474e362dde35dcb92ec66
zachary <andywu03242000@gmail.com>
2024-09-26 10:45:57 +0800

    UPDATE: Card scale animation

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,9 +1,6 @@
 function CountryCard() {
   return (
-    <div
-      className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]
-        scale-75 animate-cardScale"
-    ></div>
+    <div className="w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]"></div>
   )
 }
 
