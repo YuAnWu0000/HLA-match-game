422eafd27bc98912290b96ddcf151adfb734760c
zachary <andywu03242000@gmail.com>
2024-09-27 10:16:42 +0800

    ADD: Card front css

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -6,12 +6,22 @@ function CountryCard({ order, className }) {
   return (
     <div
       className={twMerge(
-        `w-[14rem] h-[21rem] bg-contain bg-[url('/images/card/country_card_back.png')]
-        [transform-style:preserve-3d;] translate-y-[-50rem] animate-cardDrop
-        [--delayTime:${3 + (order - 1) * 0.5}s]`,
+        `w-[14rem] h-[21rem] [transform-style:preserve-3d;] translate-y-[-50rem]
+        animate-cardDrop [--delayTime:${3 + (order - 1) * 0.5}s]`,
         className
       )}
-    ></div>
+    >
+      {/* Card Back */}
+      <div
+        className="absolute top-0 right-0 w-full h-full bg-contain
+          bg-[url('/images/card/country_card_back.png')]"
+      ></div>
+      {/* Card Front */}
+      <div
+        className="absolute top-0 right-0 w-full h-full bg-contain
+          bg-[url('/images/card/country_card_front.png')]"
+      ></div>
+    </div>
   )
 }
 
