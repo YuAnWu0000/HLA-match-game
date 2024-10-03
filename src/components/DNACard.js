32f3cfd59685854f39029c1014ebafb8e9d62f43
Zachary Wu <andywu03242000@gmail.com>
2024-10-03 20:14:56 +0800

    ADD: DNA card

--- src/components/DNACard.js
+++ src/components/DNACard.js
@@ -1,6 +1,27 @@
-function DNACard() {
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import { twMerge } from 'tailwind-merge'
+
+function DNACard({ className }) {
+  const [isCardOpen, setIsCardOpen] = useState()
   return (
-    <div className="w-[12rem] h-[18rem] bg-contain bg-[url('/images/card/dna_card_back.png')]"></div>
+    <div
+      className={twMerge(
+        "w-[12rem] h-[18rem] bg-contain bg-[url('/images/card/dna_card_back.png')]",
+        className
+      )}
+    >
+      <div
+        className={twMerge(
+          'w-full h-full [transform-style:preserve-3d;]',
+          isCardOpen && 'animate-cardFlip'
+        )}
+      ></div>
+    </div>
   )
+}
+
+DNACard.propTypes = {
+  className: PropTypes.string
 }
 export default DNACard
