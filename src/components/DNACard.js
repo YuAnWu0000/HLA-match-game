736dce0eddbe058fb1a24f286bbca1c049ebeae1
Zachary Wu <andywu03242000@gmail.com>
2024-10-03 20:22:40 +0800

    UPDATE: DNA card

--- src/components/DNACard.js
+++ src/components/DNACard.js
@@ -5,6 +5,7 @@ import { twMerge } from 'tailwind-merge'
 function DNACard({ className }) {
   const [isCardOpen, setIsCardOpen] = useState()
   function cardBackClick() {
+    console.log('!!')
     setIsCardOpen(true)
   }
 
@@ -22,6 +23,19 @@ function DNACard({ className }) {
             bg-[url('/images/card/dna_card_back.png')] [backface-visibility:hidden;]"
           onClick={cardBackClick}
         ></div>
+        {/* Card Front */}
+        <div
+          className="absolute top-0 right-0 w-full h-full [backface-visibility:hidden;]
+            [transform:rotate3d(0,1,0,180deg);] rounded-[1.25rem] overflow-hidden"
+          onClick={() => setIsCardOpen(false)}
+        >
+          {/* Card frame */}
+          <img
+            src="/images/card/dna_card_front_frame.jpg"
+            alt=""
+            className="absolute top-0 right-0 w-full h-full"
+          />
+        </div>
       </div>
     </div>
   )
