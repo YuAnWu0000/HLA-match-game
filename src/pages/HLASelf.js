42b153474e6b9fb11e42fefaa8e237326052e648
zachary <andywu03242000@gmail.com>
2024-10-07 13:20:51 +0800

    ADD: Self HLA page

---
+++ src/pages/HLASelf.js
@@ -0,0 +1,13 @@
+function HLASelf() {
+  return (
+    <div className="w-full h-full">
+      <div
+        className="absolute top-0 right-0 -z-50 w-full h-full bg-repeat bg-[length:140%_auto]
+          bg-[center_-25rem] bg-[url('/images/hla_self.jpg')]"
+      ></div>
+      <div className="absolute top-0 right-0 w-full h-full bg-[rgba(0,0,0,0.7)]"></div>
+    </div>
+  )
+}
+
+export default HLASelf
