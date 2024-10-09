37f7720ffdb83a81d78d5e8a55d73d8c2b10180d
zachary <andywu03242000@gmail.com>
2024-10-09 17:10:17 +0800

    UPDATE: Pairing Result

--- src/components/PairingResult.js
+++ src/components/PairingResult.js
@@ -10,32 +10,60 @@ function PairingResult() {
   return (
     showPairingResult && (
       <div className="absolute top-0 right-0 w-full h-full z-10">
-        <div className="absolute top-0 right-0 w-full h-full bg-[rgba(0,0,0,0.9)]"></div>
+        <div className="absolute top-0 right-0 w-full h-full bg-[rgba(0,0,0,0.8)]"></div>
         <img
-          src="/images/pairing_result.png"
-          className="relative top-0 w-auto h-full mx-auto"
+          src="/images/pairing_bg.png"
+          className="absolute top-0 right-1/2 translate-x-1/2 w-[45%] h-auto"
           alt=""
         />
-        {/* First HLA */}
-        <div className="absolute top-[27%] right-[39.5%] w-[11rem] text-center">
-          <img src={HLA_MAP['r1'].image} alt="" className="w-full h-auto" />
-          <h1
-            className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
-              tracking-widest mt-3"
-          >
-            {HLA_MAP['r1'].name}
-          </h1>
+        {/* HLA area */}
+        <div
+          className="relative top-[33%] m-auto w-[40.5%] h-[46%] bg-white border border-black flex
+            items-center"
+        >
+          {/* First HLA */}
+          <div className="w-[11rem] text-center mx-2 border p-4 border-black">
+            <img src={HLA_MAP['r1'].image} alt="" className="w-full h-auto" />
+            <h1
+              className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
+                tracking-wide mt-3"
+            >
+              {HLA_MAP['r1'].name}
+            </h1>
+          </div>
+          {/* Second HLA */}
+          <div className="w-[11rem] text-center mx-2 border p-4 border-black">
+            <img src={HLA_MAP['b2'].image} alt="" className="w-full h-auto" />
+            <h1
+              className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
+                tracking-wide mt-3"
+            >
+              {HLA_MAP['b2'].name}
+            </h1>
+          </div>
+          <div className="w-[2px] h-full bg-black"></div>
+          {/* First HLA */}
+          <div className="w-[11rem] text-center mx-2 border p-4 border-black">
+            <img src={HLA_MAP['r1'].image} alt="" className="w-full h-auto" />
+            <h1
+              className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
+                tracking-wide mt-3"
+            >
+              {HLA_MAP['r1'].name}
+            </h1>
+          </div>
+          {/* Second HLA */}
+          <div className="w-[11rem] text-center mx-2 border p-4 border-black">
+            <img src={HLA_MAP['b2'].image} alt="" className="w-full h-auto" />
+            <h1
+              className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
+                tracking-wide mt-3"
+            >
+              {HLA_MAP['b2'].name}
+            </h1>
+          </div>
         </div>
-        {/* Second HLA */}
-        <div className="absolute top-[50%] right-[2rem] w-[8rem] text-center">
-          <img src={HLA_MAP['b2'].image} alt="" className="w-full h-auto" />
-          <h1
-            className="font-semibold text-primary text-3xl [text-shadow:1px_1px_0px_#BFBFBF]
-              tracking-widest mt-2"
-          >
-            {HLA_MAP['b2'].name}
-          </h1>
-        </div>
+
         {/* Next step */}
         <div
           className="group absolute right-[1rem] bottom-[1rem] flex items-center cursor-pointer
