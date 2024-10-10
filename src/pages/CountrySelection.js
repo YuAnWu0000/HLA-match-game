44bfd0d943024341416085b8e6f6bb2593a1221c
Zachary Wu <andywu03242000@gmail.com>
2024-10-10 21:33:47 +0800

    FIX: Table position

--- src/pages/CountrySelection.js
+++ src/pages/CountrySelection.js
@@ -7,7 +7,7 @@ function CountrySelection() {
     <div className="w-full h-full [perspective:1000px;] animate-openLight brightness-[0.4]">
       {/* Table background */}
       <div
-        className="absolute top-0 right-0 -z-50 w-full h-full bg-no-repeat bg-[length:100%_auto]
+        className="absolute top-0 right-0 -z-50 w-full h-full bg-repeat bg-[length:100%_auto]
           bg-[center_0rem] bg-[url('/images/bg_country.jpg')] animate-bgZoomIn"
       ></div>
       {/* <img
